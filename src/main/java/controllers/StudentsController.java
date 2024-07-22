a6e673b298990216f78b59691b7788af9293e5c8
galexal <galexal@yandex.ru>
2024-07-22 11:37:23 +0400

    код с занятия 39

--- src/main/java/controllers/StudentsController.java
+++ src/main/java/controllers/StudentsController.java
@@ -1,5 +1,7 @@
 package controllers;
 
+import db.DBManager;
+import entity.Student;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -7,30 +9,15 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
-import java.sql.*;
+import java.util.List;
 
 @WebServlet(name = "StudentsController", urlPatterns = "/students")
 public class StudentsController extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        // Соединение с БД
-        try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
-            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/students" +
-                    "?user=root&password=P@$5w0rd");
-            Statement statement = connection.createStatement();
-            ResultSet result = statement.executeQuery("SELECT * FROM students.student;");
-
-            while (result.next()) {
-                System.out.println(result.getInt("id") + " " + result.getString("name") + " "
-                        + result.getString("surname"));
-            }
-            connection.close();
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-
+        List<Student> students = DBManager.getAllActiveStudents();
+        req.setAttribute("students", students);
         req.getRequestDispatcher("WEB-INF/jsp/students.jsp").forward(req, resp);
     }
 }
---
+++ src/main/java/db/DBManager.java
@@ -0,0 +1,58 @@
+package db;
+
+import entity.Group;
+import entity.Student;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+public class DBManager {
+
+    private static Statement statement;
+    private static final String ID = "id";
+    private static final String NAME = "name";
+    private static final String SURNAME = "surname";
+    private static final String DATE = "date";
+    private static final String GROUP = "group";
+
+    static {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/students" +
+                    "?user=root&password=P@$5w0rd");
+            statement = connection.createStatement();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static List<Student> getAllActiveStudents() {
+        List<Student> students = new ArrayList<>();
+        try {
+            ResultSet result = statement.executeQuery("SELECT s.id, surname, name, date, g.group " +
+                    "FROM students.student AS s\n" +
+                    "JOIN groupp AS g ON s.id_group=g.id\n" +
+                    "WHERE status='1';");
+            while (result.next()) {
+                Student student = new Student();
+                student.setId(result.getInt(ID));
+                student.setName(result.getString(NAME));
+                student.setSurname(result.getString(SURNAME));
+                student.setDate(result.getDate(DATE));
+
+                Group group = new Group();
+                group.setName(result.getString(GROUP));
+                student.setGroup(group);
+
+                students.add(student);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return students;
+    }
+}
